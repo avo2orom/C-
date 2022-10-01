698a6719421a2cbd2b317bfa6ee2161ad977ba73
Alex <aov2orom@yandex.ru>
2022-10-01 15:16:03 +0300

    Add Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+// Ex003v1 Sum of 2 intereg numbers
+int numberA = 3; //int = целое число
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+*/
+
+/*
+// Ex003v2 Sum of 2 intereg numbers
+int numberA = 3; //int = целое число
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
+
+// Ex003v3 Sum of 2 intereg numbers
+Console.WriteLine("Enter a integer 1");
+int numberA = Convert.ToInt32(Console.ReadLine()); //int = целое число
+Console.WriteLine("Enter a integer 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA + numberB;
+Console.WriteLine("Checkout sum:");
+Console.WriteLine(result);
