111ec95eac153189a06e3b706ba67e37f826ce66
Alex <aov2orom@yandex.ru>
2022-10-01 15:27:41 +0300

    Add Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,8 @@
+﻿// Ex004v1 Division of 2 intereg numbers
+Console.WriteLine("Enter a integer 1");
+double numberA = Convert.ToInt32(Console.ReadLine()); //int = целое число, double - дробь
+Console.WriteLine("Enter a integer 2");
+double numberB = Convert.ToInt32(Console.ReadLine());
+double result = numberA / numberB;
+Console.WriteLine("Checkout division:");
+Console.WriteLine(result);
