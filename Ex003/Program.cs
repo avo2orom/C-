2de31349df6e94265150d581ef89d3ef4f4f2cc9
Alex <aov2orom@yandex.ru>
2022-10-02 11:47:03 +0300

    Add sum of random int in EX003

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -13,6 +13,7 @@ int result = numberA + numberB;
 Console.WriteLine(result);
 */
 
+/*
 // Ex003v3 Sum of 2 intereg numbers
 Console.WriteLine("Enter a integer 1");
 int numberA = Convert.ToInt32(Console.ReadLine()); //int = целое число
@@ -21,3 +22,11 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 int result = numberA + numberB;
 Console.WriteLine("Checkout sum:");
 Console.WriteLine(result);
+*/
+
+// Ex003v4 Sum of 2 RANDOM intereg numbers
+int numberA = new Random().Next(1, 10); //Next() - уровни дидапозона данных!!! Max число = 9 (max-1)
+Console.WriteLine(numberA); // выводит значения рандомного числа 1
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB); // выводит значения рандомного числа 2
+Console.WriteLine(numberA + numberB); // Выводит резик
