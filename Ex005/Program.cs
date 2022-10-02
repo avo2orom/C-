1947ca8807eca884ff70db574ad902c6047f1066
Alex <aov2orom@yandex.ru>
2022-10-02 12:02:56 +0300

    Add Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ex005v1 Selection with condition
+Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "alex") // ToLower - переводит все символы строки в нижний регистр
+{
+    Console.WriteLine("Hello, dear Alex!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
