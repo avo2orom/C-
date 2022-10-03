e0d2495e960acffa5baf005bc72ee6480e7f9937
Alex <aov2orom@yandex.ru>
2022-10-03 22:00:15 +0300

    Add Ex008 function\method

---
+++ Ex008/Program.cs
@@ -0,0 +1,31 @@
+﻿// Функции(Методы) и массивы 
+int Max(int arg1, int arg2, int arg3) // Начало функции
+{
+    int result = arg1;                  //Условия функции
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+Console.WriteLine("Enter 9 integer");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int number4 = Convert.ToInt32(Console.ReadLine());
+int number5 = Convert.ToInt32(Console.ReadLine());
+int number6 = Convert.ToInt32(Console.ReadLine());
+int number7 = Convert.ToInt32(Console.ReadLine());
+int number8 = Convert.ToInt32(Console.ReadLine());
+int number9 = Convert.ToInt32(Console.ReadLine());
+
+/* Варик 1
+int maxnumber1 = Max(number1, number2, number3);
+int maxnumber2 = Max(number4, number5, number6);
+int maxnumber3 = Max(number7, number8, number9);
+int supermax = Max(maxnumber1, maxnumber2, maxnumber3);
+*/
+
+//Варик 2, компактный
+int supermax = Max(Max(number1, number2, number3), Max(number4, number5, number6), Max(number7, number8, number9));
+Console.WriteLine(supermax);
